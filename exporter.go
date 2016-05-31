ca5444f6bfd81b897740018acfad59f6f22f886b
moznion <moznion@gmail.com>
2016-05-31 15:10:48 +0900

    Refactor

--- exporter.go
+++ exporter.go
@@ -79,20 +79,26 @@ func (e *exporter) Describe(ch chan<- *prometheus.Desc) {
 func (e *exporter) Collect(ch chan<- prometheus.Metric) {
 	e.mut.Lock() // To protect metrics from concurrent collects.
 	defer e.mut.Unlock()
-	defer func() {
-		e.queueStatus.Collect(ch)
-		e.processed.Collect(ch)
-		e.failed.Collect(ch)
-		e.totalWorkers.Collect(ch)
-		e.activeWorkers.Collect(ch)
-		e.idleWorkers.Collect(ch)
-	}()
 
 	if e.timer != nil {
 		// guarded
+		e.notifyToCollect(ch)
 		return
 	}
 
+	if err := e.collect(ch); err != nil {
+		e.incrementFailures(ch)
+	}
+
+	e.timer = time.AfterFunc(time.Duration(e.config.GuardIntervalMillis)*time.Millisecond, func() {
+		// reset timer
+		e.mut.Lock()
+		defer e.mut.Unlock()
+		e.timer = nil
+	})
+}
+
+func (e *exporter) collect(ch chan<- prometheus.Metric) error {
 	resqueNamespace := e.config.ResqueNamespace
 
 	redisConfig := e.config.Redis
@@ -106,39 +112,34 @@ func (e *exporter) Collect(ch chan<- prometheus.Metric) {
 
 	queues, err := redis.SMembers(fmt.Sprintf("%s:queues", resqueNamespace)).Result()
 	if err != nil {
-		e.incrementFailures(ch)
-		return
+		return err
 	}
 
 	for _, q := range queues {
 		n, err := redis.LLen(fmt.Sprintf("%s:queue:%s", resqueNamespace, q)).Result()
 		if err != nil {
-			e.incrementFailures(ch)
-			return
+			return err
 		}
 		e.queueStatus.WithLabelValues(q).Set(float64(n))
 	}
 
 	processed, err := redis.Get(fmt.Sprintf("%s:stat:processed", resqueNamespace)).Result()
 	if err != nil {
-		e.incrementFailures(ch)
-		return
+		return err
 	}
 	processedCnt, _ := strconv.ParseFloat(processed, 64)
 	e.processed.Set(processedCnt)
 
 	failed, err := redis.Get(fmt.Sprintf("%s:stat:failed", resqueNamespace)).Result()
 	if err != nil {
-		e.incrementFailures(ch)
-		return
+		return err
 	}
 	failedCnt, _ := strconv.ParseFloat(failed, 64)
 	e.failed.Set(failedCnt)
 
 	workers, err := redis.SMembers(fmt.Sprintf("%s:workers", resqueNamespace)).Result()
 	if err != nil {
-		e.incrementFailures(ch)
-		return
+		return err
 	}
 	e.totalWorkers.Set(float64(len(workers)))
 
@@ -155,15 +156,21 @@ func (e *exporter) Collect(ch chan<- prometheus.Metric) {
 	e.activeWorkers.Set(float64(activeWorkers))
 	e.idleWorkers.Set(float64(idleWorkers))
 
-	e.timer = time.AfterFunc(time.Duration(e.config.GuardIntervalMillis)*time.Millisecond, func() {
-		// reset timer
-		e.mut.Lock()
-		defer e.mut.Unlock()
-		e.timer = nil
-	})
+	e.notifyToCollect(ch)
+
+	return nil
 }
 
 func (e *exporter) incrementFailures(ch chan<- prometheus.Metric) {
 	e.scrapeFailures.Inc()
 	e.scrapeFailures.Collect(ch)
+}
+
+func (e *exporter) notifyToCollect(ch chan<- prometheus.Metric) {
+	e.queueStatus.Collect(ch)
+	e.processed.Collect(ch)
+	e.failed.Collect(ch)
+	e.totalWorkers.Collect(ch)
+	e.activeWorkers.Collect(ch)
+	e.idleWorkers.Collect(ch)
 }
